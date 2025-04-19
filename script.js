23d824b3d1dab312db05219ec674630621dafff6
VyacheslavKoryakin <kkvvvv02@gmail.com>
2025-04-19 22:28:18 +0300

    HW-9

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
+class LibraryItem {
+    
+    constructor(author, title, year) {
+        this.author = author;
+        this.title = title;
+        this.year = year;
+    }
+    itemInfo(){
+        throw new Error("");
+    }
 
-//1
-
-let  number =8;
-if(typeof(number)=="number"){
-if(number%2==0){
-    console.log(`Число ${number} является четным`)
-}else{
-console.log(`Число ${number} является нечетным`)
 }
-}else{
-    console.log(`Ошибка ${number} не является числом`)
+
+
+class Book extends LibraryItem {
+    
+    
+    constructor(author, title, year, info) {
+      
+        super(author, title, year) 
+        this.info = info;
+
+    }
+
+    itemInfo(){
+        console.log(`Информация о книге: автор: ${this.author}, название: "${this.title}", год выпуска: ${this.year}, краткое содержание: ${this.info}`);
+        
+    }
+}
+class Jornal extends LibraryItem {
+    
+    
+    constructor(author, title, year,  article ,info) {
+      
+        super(author, title, year) 
+        this.article= article;
+        this.info = info;
+
+    }
+
+    itemInfo(){
+        console.log(`Информация о журнале: название: "${this.title}" ,год выпуска: ${this.year}, автор: ${this.author}, название статьи: ${this.article}, краткое содержание: ${this.info}`);
+        
+    }
 }
 
 
 
-//2
+const book = new Book("Д. Роалинг", "Гарри Поттер", 2000, " Книга о юном волшебнике...");
+console.log(book.itemInfo());
 
-let valute = prompt('введите сумму долларах');
-let courseDollarRubl=85;
-let result = valute*courseDollarRubl;
 
-alert(`Сумма в рублях равна  ${result} р.`)
+const jornal = new Jornal("Э. Смирнов ","Юный техник" , 1987,"Радио-приемник своими руками"," Статья о том, как можно собрать ....");
+console.log(jornal.itemInfo());
