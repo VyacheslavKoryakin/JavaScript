1727ddd566a9ae99ef6d1469645a6c0a54c29daa
VyacheslavKoryakin <kkvvvv02@gmail.com>
2025-04-27 19:09:14 +0300

    HW-16
    HW-16

--- script.js
+++ script.js
@@ -1,23 +1,124 @@
+let textarea = document.querySelector('#fromJSON')
+let textarea2 = document.querySelector('#name')
+let lowPopulation = document.querySelector('#but1')
+let middlePopulation = document.querySelector('#but2')
+let bigPopulation = document.querySelector('#but3')
+let veryBigPopulation = document.querySelector('#but4')
+let elements = [];
+let data;
+document.addEventListener('DOMContentLoaded', async function (params) {
+    const jsonFilePath = "file.json";
+    const response = await fetch(jsonFilePath);
+    if (!response.ok) {
 
-//1
+        console.error('Error take file')
+    }
+    data = await response.json();
+})
 
-let  number =8;
-if(typeof(number)=="number"){
-if(number%2==0){
-    console.log(`Число ${number} является четным`)
-}else{
-console.log(`Число ${number} является нечетным`)
-}
-}else{
-    console.log(`Ошибка ${number} не является числом`)
-}
+lowPopulation.addEventListener('click', async function (params) {
+    if (elements.length > 0) {
+        for (let i = 0; i < elements.length;) {
+            let lastElem = elements.pop()
+            console.log(lastElem)
 
 
+            document.querySelector('body').removeChild(lastElem)
+        }
+    }
+    for (let index = 0; index < data.cityes.length; index++) {
+        if (data.cityes[index].population < 50000) {
 
-//2
+            let box = document.createElement("div")
+            box.className = data.cityes[index].name;
+            document.querySelector('body').appendChild(box)
 
-let valute = prompt('введите сумму долларах');
-let courseDollarRubl=85;
-let result = valute*courseDollarRubl;
+            let elem = document.createElement("textarea")
 
-alert(`Сумма в рублях равна  ${result} р.`)
+            elem.innerText = data.cityes[index].name;
+
+            document.querySelector("." + data.cityes[index].name).appendChild(elem)
+            elements.push(box)
+        }
+    }
+})
+middlePopulation.addEventListener('click', async function (params) {
+    if (elements.length > 0) {
+        for (let i = 0; i < elements.length;) {
+            let lastElem = elements.pop()
+            console.log(lastElem)
+
+
+            document.querySelector('body').removeChild(lastElem)
+        }
+    }
+    for (let index = 0; index < data.cityes.length; index++) {
+        if (data.cityes[index].population > 50000 && data.cityes[index].population < 100000) {
+
+            let box = document.createElement("div")
+            box.className = data.cityes[index].name;
+            document.querySelector('body').appendChild(box)
+
+            let elem = document.createElement("textarea")
+
+            elem.innerText = data.cityes[index].name;
+
+            document.querySelector("." + data.cityes[index].name).appendChild(elem)
+            elements.push(box)
+        }
+    }
+})
+
+bigPopulation.addEventListener('click', async function (params) {
+    if (elements.length > 0) {
+        for (let i = 0; i < elements.length;) {
+            let lastElem = elements.pop()
+            console.log(lastElem)
+
+
+            document.querySelector('body').removeChild(lastElem)
+        }
+    }
+    for (let index = 0; index < data.cityes.length; index++) {
+        if (data.cityes[index].population > 100000 && data.cityes[index].population < 250000) {
+
+            let box = document.createElement("div")
+            box.className = data.cityes[index].name;
+            document.querySelector('body').appendChild(box)
+
+            let elem = document.createElement("textarea")
+
+            elem.innerText = data.cityes[index].name;
+
+            document.querySelector("." + data.cityes[index].name).appendChild(elem)
+            elements.push(box)
+        }
+    }
+})
+
+veryBigPopulation.addEventListener('click', async function (params) {
+    if (elements.length > 0) {
+        for (let i = 0; i < elements.length;) {
+            let lastElem = elements.pop()
+            console.log(lastElem)
+
+
+            document.querySelector('body').removeChild(lastElem)
+        }
+    }
+    for (let index = 0; index < data.cityes.length; index++) {
+        if (data.cityes[index].population > 250000) {
+
+            let box = document.createElement("div")
+            box.className = data.cityes[index].name;
+            document.querySelector('body').appendChild(box)
+
+            let elem = document.createElement("textarea")
+
+            elem.innerText = data.cityes[index].name;
+
+            document.querySelector("." + data.cityes[index].name).appendChild(elem)
+            elements.push(box)
+        }
+    }
+})
